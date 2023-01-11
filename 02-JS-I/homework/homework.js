226f1b64117057b0ebcc764b77ae2fdfe1385e19
Falbornozd <alborneti2400@gmail.com>
2023-01-11 10:51:22 -0500

    correccion

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -103,11 +103,6 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if( num 2 === 0) {
-    return true;
-  } else {
-    return false;
-  }
   }
 
 
@@ -179,6 +174,7 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  'str!'
 }
 
 function combinarNombres(nombre, apellido) {
